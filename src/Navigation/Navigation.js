cd90cf131f78e12bad38c764c31c555ba4670658
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2021-12-31 08:31:34 +0300

    Navigation folder structure setup

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import routes from './routes';
 
 export default function Navigation() {
-  return <View></View>;
+  const Tab = createBottomTabNavigator();
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        {/*login new activity */}
+        <Tab.Screen name={routes.HOME} component={HomeScreen} />
+        <Tab.Screen name={routes.DASHBOARD} component={SettingsScreen} />
+        <Tab.Screen name={routes.HISTORY} component={HomeScreen} />
+        <Tab.Screen name={routes.LEADERBOARD} component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
---
+++ src/Navigation/routes.js
@@ -0,0 +1,9 @@
+export default {
+  HOME: 'Home',
+  DASHBOARD: 'Dashboard',
+  HISTORY: 'History',
+  LEADERBOARD: 'LeaderBoard',
+  LOGIN: 'Login',
+  REGISTER: 'Register',
+  HS_DETAILS: 'HsDetails',
+};
