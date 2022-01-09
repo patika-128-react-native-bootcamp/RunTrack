1d76fa1ec36dc8b523fb788131056f9ef0d99e12
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2022-01-09 22:22:33 +0300

    History Screen added.

--- src/Screens/HistoryScreen/HistoryScreen.js
+++ src/Screens/HistoryScreen/HistoryScreen.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import {firebase} from '@react-native-firebase/database';
+import {useNavigation} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, FlatList, Button, ActivityIndicator} from 'react-native';
+import useDbRead from '../../helpers/DbHelper/DbRead';
+import routes from '../../Navigation/routes';
+import ButtonComponent from '../../components/Button';
+import styles from './HistoryScreen.styles';
 
 export default function HistoryScreen() {
+  const navigation = useNavigation();
+  const [dbValue, setDbValue] = useState();
+  const userId = firebase.auth().currentUser.uid;
+  const data = useDbRead('/users/' + userId + '/');
+  useEffect(() => {
+    const result = !!data && Object.keys(data).map(key => [key, data[key]]);
+    setDbValue(result);
+  }, [data]);
+  function renderItem(item) {
+    return (
+      <View>
+        {!!dbValue ? (
+          <View style={styles.list}>
+            <Text style={styles.text}>{item.item[0]}</Text>
+            <ButtonComponent
+              text="Details"
+              onPress={() => {
+                showDetails(item);
+              }}
+              theme={'secondary'}
+            />
+          </View>
+        ) : (
+          <ActivityIndicator size="large" color="#00ff00" />
+        )}
+      </View>
+    );
+  }
+
+  function showDetails(detail) {
+    navigation.navigate(routes.HS_DETAILS, {detail});
+  }
   return (
     <View>
-      <Text></Text>
+      <FlatList data={dbValue} renderItem={renderItem} />
     </View>
   );
 }
