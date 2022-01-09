361bcfe2938d2492d8fab85be517de8e52999f23
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2022-01-09 22:22:13 +0300

    HistoryDetails Screen added.

--- src/Screens/HistoryDetailsScreen/HistoryDetailsScreen.js
+++ src/Screens/HistoryDetailsScreen/HistoryDetailsScreen.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import MapComponent from '../../components/Map/MapComponent';
+import styles from './HistoryDetailsScreen.styles';
 
-export default function HistoryDetailsScreen() {
+export default function HistoryDetailsScreen({route}) {
+  const dbValue = route.params;
+  let historyDetails =
+    dbValue.detail.item[1][Object.keys(dbValue.detail.item[1])[0]];
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <MapComponent
+        initCoords={historyDetails.posHistory[0]}
+        location={
+          historyDetails.posHistory[historyDetails.posHistory.length - 1]
+        }
+        posHistory={historyDetails.posHistory}
+      />
+
+      <Text style={styles.text}>DENEME</Text>
     </View>
   );
 }
---
+++ src/Screens/HistoryDetailsScreen/HistoryDetailsScreen.styles.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {flex: 1},
+  mapStyle: {
+    flex: 1,
+  },
+  text: {position: 'absolute', bottom: 100},
+});
