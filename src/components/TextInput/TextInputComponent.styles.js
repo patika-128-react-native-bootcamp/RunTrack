2076291d49cf44c56c9989c6244d4163c944c50c
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2022-01-09 22:17:34 +0300

    Text Input Component updated.

--- src/components/TextInput/TextInputComponent.styles.js
+++ src/components/TextInput/TextInputComponent.styles.js
@@ -4,8 +4,18 @@ export default StyleSheet.create({
   input: {
     height: 50,
     width: Dimensions.get('window').width - 40,
-    margin: 20,
     borderColor: 'black',
     borderWidth: 2,
+    borderRadius: 10,
+    paddingLeft: 20,
+    fontSize: 16,
+    marginBottom: 15,
+  },
+  container: {
+    marginHorizontal: 25,
+  },
+  text: {
+    marginVertical: 10,
+    fontSize: 16,
   },
 });
