aa4f0ffc5e6b7003b4ecbe5f3b8319c7c6504822
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2022-01-10 09:28:36 +0300

    Bug fix

--- src/components/ActivityData/ActivityData.js
+++ src/components/ActivityData/ActivityData.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import WeatherComponent from '../Weather/WeatherComponent';
 
 export default function ActivityData({distance, AvgSpeed}) {
   return (
     <View>
       <Text>Total Distance: {distance}</Text>
-      <Text>Average Speed:{Math.round(AvgSpeed)}</Text>
+      <Text>
+        Average Speed:{Math.round(AvgSpeed) ? Math.round(AvgSpeed) : '0'}
+      </Text>
     </View>
   );
 }
